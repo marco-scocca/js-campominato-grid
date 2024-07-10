cd6dd7005c88227c3552f4164606dc5d2c08c71f
marco-scocca <marcoscocca1991@gmail.com>
2024-07-10 08:04:41 +0200

    creo ciclo per generare le 100 caselle

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 function createSingleSquare(num) {
     const square = document.createElement('div');
     square.classList.add('square');
-    square.innerText = num;
+    square.innerText = num + 1;
 
 }
 
 const button = document.getElementById('play');
 button.addEventListener('click', function () {
     const grid = document.getElementById('grid');
-
+    for (let i = 0; i < 100; i++) {
+        createSingleSquare(i)
+    }
 });
